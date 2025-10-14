4999d085811d4332b8153ae1c90245dcbd1cac25
Antoni Kobusinski <60139174+akobusinski@users.noreply.github.com>
2025-10-14 18:40:44 +0200

    Implement PartialEq for Error

--- transmittable/src/lib.rs
+++ transmittable/src/lib.rs
@@ -22,6 +22,18 @@ pub enum Error {
     InvalidEnumVariant,
 }
 
+impl PartialEq for Error {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Error::IOError(e1),         Error::IOError(e2))         => e1.kind() == e2.kind() && e1.to_string() == e2.to_string(),
+            (Error::Utf8DecodeError(e1), Error::Utf8DecodeError(e2)) => e1 == e2,
+            (Error::InvalidBoolean(b1),  Error::InvalidBoolean(b2))  => b1 == b2,
+            (Error::InvalidEnumVariant,  Error::InvalidEnumVariant)  => true,
+            _ => false,
+        }
+    }
+}
+
 pub type Result<T> = std::result::Result<T, Error>;
 
 pub trait Transmittable {
